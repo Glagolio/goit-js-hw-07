66236954d314517e664f794a99a50d3f94405ed2
Mykhailo Shyhin <johnglagol@gmail.com>
2022-06-18 13:22:59 +0300

    task-1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const listGalleryEl = galleryItems.map(image =>
+    `<div class="gallery__item>
+        <a class="gallery__link href="${image.original}">
+            <img 
+                class="gallery__image"
+                src="${image.preview}"
+                alt="${image.alt}"
+            />
+        </a>
+    </div>`).join('');
+
+galleryEl.insertAdjacentHTML("beforeend", listGalleryEl);
