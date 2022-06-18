5e0810f4b6e52212f67d71083889e595de635730
Mykhailo Shyhin <johnglagol@gmail.com>
2022-06-18 18:56:34 +0300

    task-1 без додаткового рішення

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,37 +4,53 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector('.gallery');
 
+
+
 const listGalleryEl = galleryItems.map(image =>
-    `<div class="gallery__item>
-        <a class="gallery__link href="${image.original}">
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
             <img 
                 class="gallery__image"
                 src="${image.preview}"
                 data-source="${image.original}"
                 alt="${image.description}"
+                
             />
         </a>
     </div>`).join('');
 
 
+
+
 galleryEl.insertAdjacentHTML("beforeend", listGalleryEl);
 
 galleryEl.addEventListener('click', evt => {
+    evt.preventDefault()
+    
     const eventTarget = evt.target
+
     if (eventTarget.nodeName !== "IMG") {
         return
     };
 
     openModal(eventTarget);
-   
-}
-);
+    });
+
 
 
 function openModal(event) {
     const instance = basicLightbox.create(`
     <img src="${event.dataset.source}" width="800" height="600">
 `)
-    
     instance.show()
 };
+    
+
+// function clickOnEsc(event) {
+//     console.log (event.code)
+//         if (event.code === 'Escape') {
+//             instance.close()
+//         }
+    
+// }
+
